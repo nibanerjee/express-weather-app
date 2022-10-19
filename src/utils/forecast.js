9df08ac2619be9a4e94d2ccf88c4815c1a1bc3a5
Nilanjan Banerjee <nilanjanbanerjee@Nilanjans-MacBook-Air.local>
2022-10-20 02:33:18 +0530

    modified forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' Today\'s high is ' + body.daily.data[0].temperatureMax + ' and low is ' + body.daily.data[0].temperatureMin + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
